da0d204f72f42b60473afa5e63443da434f188fa
hugotallys <htmo@ic.ufal.com>
2022-04-05 17:56:26 -0300

    formata codigo

--- ControlLib.py
+++ ControlLib.py
@@ -1,148 +1,120 @@
 import asyncio
 import websockets
 from collections import deque
-from math import *
-import numpy as np
 import time
+import math
+
 
 class Control:
-	def __init__(self, T=0.3, order=3):
-		self.T = T
-		self._e = deque([0]*order)
-		self._u = deque([0]*order)
-		self._y = deque([0]*order)
-		self._r = deque([0]*order)
-		self.currU = 0
-		self.time = 0.0
 
+    def __init__(self, T=0.3, order=3):
+        self.T = T
+        self._e = deque([0] * order)
+        self._u = deque([0] * order)
+        self._y = deque([0] * order)
+        self._r = deque([0] * order)
+        self.currU = 0
+        self.time = 0.0
 
+    def reference(self, ref):
+        self._r.rotate(-1)
+        self._r[-1] = ref
 
-	def reference(self, ref):
-		self._r.rotate(-1)
-		self._r[-1] = ref
+    def measured(self, y):
+        error = self._r[-1] - y
 
+        self._e.rotate(-1)
+        self._e[-1] = error
 
+        self._y.rotate(-1)
+        self._y[-1] = y
 
-	def measured(self, y):
+    def control(self):
+        return 0
 
-		error = self._r[-1] - y
+    def apply(self, controlSignal):
+        self._u.rotate(-1)
+        self._u[-1] = controlSignal
 
-		self._e.rotate(-1)
-		self._e[-1] = error
+    def u(self, index=0):
+        return self._u[index]
 
-		self._y.rotate(-1)
-		self._y[-1] = y
+    def e(self, index=0):
+        return self._e[index - 1]
 
+    def r(self, index=0):
+        return self._r[index - 1]
 
-	def control(self):
-
-		return 0
-
-
-	def apply(self, controlSignal):
-
-		self._u.rotate(-1)
-		self._u[-1] = controlSignal
-
-
-	def u(self, index = 0):
-		return self._u[index]
-	def e(self, index = 0):
-		return self._e[index-1]
-	def r(self, index = 0):
-		return self._r[index-1]
-	def y(self, index = 0):
-		return self._y[index-1]
-
+    def y(self, index=0):
+        return self._y[index - 1]
 
 
 class RemoteControl:
 
-	def __init__(self, controller, verbose = False):
+    def __init__(self, controller, verbose=False):
+        self.controller = controller
+        self.verbose = verbose
 
-		self.controller = controller
-		self.verbose = verbose
+    async def serverLoop(self, websocket, path):
+        while True:
+            startTime = time.time()
 
+            await asyncio.sleep(self.controller.T)
 
+            self.controller.time += self.controller.T
 
-	async def serverLoop(self, websocket, path):
+            try:
+                print("get references") if self.verbose else None
+                await websocket.send("get references")
+                received = (await websocket.recv()).split(",")
+                print(received) if self.verbose else None
+                ref = float(received[1])
 
-		while True:
-			startTime = time.time()
+                if math.isnan(ref):
+                    ref = 0.0
 
-			await asyncio.sleep(self.controller.T)
+                print("get outputs") if self.verbose else None
+                await websocket.send("get outputs")
+                received = (await websocket.recv()).split(",")
+                print(received) if self.verbose else None
+                out = float(received[1])
 
-			self.controller.time += self.controller.T
+                self.controller.reference(ref)
 
-			try:
+                self.controller.measured(out)
 
-				print('get references') if self.verbose else None
-				references = [];
-				await websocket.send('get references')
-				received = (await websocket.recv()).split(',')
-				print(received) if self.verbose else None
-				ref = float(received[1])
+                u = self.controller.control()
 
-				if isnan(ref):
-					ref = 0.0
+                self.controller.apply(u)
 
+                print(f"u = {u}") if self.verbose else None
+                await websocket.send("set input|" + f"{u}")
 
-				print('get outputs') if self.verbose else None
-				outputs = [];
-				await websocket.send('get outputs')
-				received = (await websocket.recv()).split(',')
-				print(received) if self.verbose else None
-				out = float(received[1])
+                ellapsedTime = 0.0
 
+                while ellapsedTime < self.controller.T:
+                    time.sleep(0.0001)
+                    endTime = time.time()
+                    ellapsedTime = endTime - startTime
 
+                print(
+                    "%.4f %.4f %.4f %.4f %.4f, (%.4f)"
+                    % (
+                        self.controller.time,
+                        ref,
+                        out,
+                        u,
+                        self.controller.T,
+                        ellapsedTime,
+                    )
+                )
+            except Exception as e:
+                print(e)
+                print("System not active...") if self.verbose else None
 
+    def run(self):
 
-				self.controller.reference(ref)
+        server = websockets.serve(self.serverLoop, "localhost", 6660)
 
-				self.controller.measured(out)
-
-				u = self.controller.control()
-
-				self.controller.apply(u)
-
-
-
-
-				
-				print(f'u = {u}') if self.verbose else None
-				await websocket.send('set input|'+f"{u}")
-
-
-				ellapsedTime = 0.0
-
-				while ellapsedTime < self.controller.T:
-					time.sleep(0.0001)
-					endTime = time.time()
-					ellapsedTime = endTime - startTime
-
-
-				print('%.4f %.4f %.4f %.4f %.4f, (%.4f)'%(self.controller.time, ref, out, u, self.controller.T, ellapsedTime))				
-
-
-			except:
-				print('System not active...') if self.verbose else None
-
-	def run(self):
-
-		server = websockets.serve(self.serverLoop, "localhost", 6660)
-
-		asyncio.ensure_future(server)
-		asyncio.get_event_loop().run_forever()
-
-
-
-
-
-
-
-
-
-
-
-
-
+        asyncio.ensure_future(server)
+        asyncio.get_event_loop().run_forever()
--- test.py
+++ test.py
@@ -1,33 +1,45 @@
 #!/usr/bin/env python
 
 import asyncio
+import threading
 import websockets
 
-queue = asyncio.Queue()
+from queue import Queue
+
+queue = Queue(maxsize=100)
+
 
 async def echo(websocket):
-	while True:
-		await websocket.send("get references")
-		received = (await websocket.recv()).split(',')
-		#print(received)
-		await websocket.send("get outputs")
-		received = (await websocket.recv()).split(',')
-		#print(received)
-		await queue.put(received)
+    while True:
+        await websocket.send("get references")
+        received = (await websocket.recv()).split(",")
+        # print("Reference: " + received[1])
+        await websocket.send("get outputs")
+        received = (await websocket.recv()).split(",")
+        print("Outputs: " + received[1])
+        queue.put(received)
 
-async def view():
-	while True:
-		if not queue.empty():
-			print("oi")
-			view = await queue.get()
-			print(view)
+
+def view():
+    while True:
+        print("estou vivo")
+        if not queue.empty():
+            print("oi")
+            view = queue.get()
+            print(view)
+
 
 async def main():
-	async with websockets.serve(echo, "localhost", 6660):
-		await asyncio.Future()  # run forever
-	'''server = websockets.serve(echo, "localhost", 6660)
-	asyncio.ensure_future(server)
-	viewer = view()
-	asyncio.ensure_future(viewer)
-	asyncio.get_event_loop().run_forever()'''
-asyncio.run(main())
+    async with websockets.serve(echo, "localhost", 6660):
+        await asyncio.Future()  # run forever
+
+    # server = websockets.serve(echo, "localhost", 6660)
+    # asyncio.ensure_future(server)
+    # asyncio.get_event_loop().run_forever()
+
+
+x = threading.Thread(target=view)
+x.start()
+
+y = threading.Thread(target=main)
+y.start()
