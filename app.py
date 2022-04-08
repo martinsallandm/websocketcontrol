0f495864667e121a59ccd5b90096a84582e9c706
hugotallys <htmo@ic.ufal.com>
2022-04-08 20:58:38 -0300

    atualiza grafico dinamico

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from PyQt6 import QtWidgets, QtCore
+from PyQt6 import QtWidgets, QtCore, QtGui
 import pyqtgraph as pg
 import sys  # We need sys so that we can pass argv to QApplication
 import numpy as np
@@ -6,36 +6,19 @@ from scipy import signal
 
 from main_window import Ui_MainWindow
 
+from dynamic_plotter import DynamicPlotter
+
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, obj=None, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
 
-        self.graphWidget = pg.PlotWidget(self.centralWidget)
+        self.dynamic_plotter = DynamicPlotter(self.centralWidget, 0.01)
+
+        self.graphWidget = self.dynamic_plotter.get_plot_widget()
         self.graphWidget.setObjectName("graphWidget")
         self.graphWidgetLayout.addWidget(self.graphWidget)
-
-        self.maxAmplitude = 0.0
-        self.minAmplitude = 0.0
-        self.period = 2*np.pi
-        self.offset = 0.0
-
-        self.plot_func = self.step
-
-        self.n_points = 100
-        self.x = np.linspace(0, 5.0, self.n_points)
-        self.y = self.plot_func(self.maxAmplitude, self.period, self.offset)
-
-        self.graphWidget.setBackground('w')
-
-        pen = pg.mkPen(color=(255, 0, 0))
-        self.data_line = self.graphWidget.plot(self.x, self.y, pen=pen, )
-        self.graphWidget.setLimits(yMin=-1.0, yMax=1.0)
-        self.timer = QtCore.QTimer()
-        self.timer.setInterval(1000//24)
-        self.timer.timeout.connect(self.update_plot_data)
-        self.timer.start()
 
         self.comboBoxLoop.currentIndexChanged.connect(
             self.change_loop
@@ -66,53 +49,30 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def change_wave_form(self, i):
         wave_form = self.comboBoxWaveForm.itemText(i)
-        self.plot_func = getattr(self, wave_form)
         if wave_form == "random":
             self.labelMaxAmplitude.setText("Max Amplitude")
             self.doubleBoxMinAmplitude.setEnabled(True)
         else:
             self.labelMaxAmplitude.setText("Amplitude")
             self.doubleBoxMinAmplitude.setEnabled(False)
+        self.dynamic_plotter.set_plot_func(wave_form)
 
     def change_max_amplitude(self, value):
-        self.maxAmplitude = value
+        self.dynamic_plotter.set_maxAmplitude(value)
 
     def change_min_amplitude(self, value):
-        self.mminAmplitude = value
+        self.dynamic_plotter.set_minAmplitude(value)
 
     def change_period(self, value):
-        self.period = value
+        self.dynamic_plotter.set_period(value)
 
     def change_offset(self, value):
-        self.offset = value
-
-    def update_plot_data(self):
-
-        self.x = np.roll(self.x, -1)  # Remove the first y element.
-        self.x[-1] = (self.x[-2] - self.x[-3]) + self.x[-2]
-        self.y = self.plot_func(self.maxAmplitude, self.period, self.offset)
-        self.data_line.setData(self.x, self.y)  # Update the data.
-
-    def step(self, amp=0.0, per=None, offset=None, min_amp=None):
-        return amp * np.ones_like(self.x) + self.offset * np.ones_like(self.x)
-
-    def sine(self, amp=0.0, per=2*np.pi, offset=0.0, min_amp=None):
-        return amp*np.sin((2*np.pi/per)*self.x) + offset*np.ones_like(self.x)
-
-    def square(self, amp=0.0, per=2*np.pi, offset=0.0, min_amp=None):
-        off = offset*np.ones_like(self.x)
-        return amp*signal.square((2*np.pi/per)*self.x) + off
-
-    def sawtooth(self, amp=0.0, per=2*np.pi, offset=0.0, min_amp=None):
-        off = offset*np.ones_like(self.x)
-        return amp*signal.sawtooth((2*np.pi/per)*self.x) + off
-
-    def random(self, amp=0.0, per=2*np.pi, offset=0.0, min_amp=None):
-        return amp * np.ones_like(self.x)
+        self.dynamic_plotter.set_offset(value)
 
 
 app = QtWidgets.QApplication(sys.argv)
 
 window = MainWindow()
+window.setWindowIcon(QtGui.QIcon("qtui/feedback.png"))
 window.show()
 app.exec()
---
+++ dynamic_plotter.py
@@ -0,0 +1,88 @@
+from pyqtgraph.Qt import QtCore
+import pyqtgraph as pg
+from scipy import signal
+
+import collections
+import time
+import math
+import numpy as np
+
+# Original script at
+# https://github.com/ap--/python-live-plotting/blob/master/plot_pyqtgraph.py
+
+
+class DynamicPlotter:
+    def __init__(
+            self, widget, sampleinterval=0.1, timewindow=10.0,
+            size=(600, 350)):
+        # Data stuff
+        self._interval = int(sampleinterval * 1000)
+        self._bufsize = int(timewindow / sampleinterval)
+        self.databuffer = collections.deque(
+            [0.0] * self._bufsize, self._bufsize)
+        self.x = np.linspace(0.0, timewindow, self._bufsize)
+        self.y = np.zeros(self._bufsize, dtype=np.float)
+        self.plt = pg.PlotWidget(widget)
+        self.plt.setYRange(-1.0, 1.0)
+        self.plt.showGrid(x=True, y=True)
+        self.plt.setLabel("left", "amplitude", "V")
+        self.plt.setLabel("bottom", "time", "s")
+        self.curve = self.plt.plot(self.x, self.y, pen=(255, 0, 0))
+
+        self.plot_func = "step"
+        self.maxAmplitude = 0.0
+        self.minAmplitude = 0.0
+        self.period = 2*np.pi
+        self.offset = 0.0
+
+        # QTimer
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.updateplot)
+        self.timer.start(self._interval)
+
+    def set_plot_func(self, value):
+        self.plot_func = value
+
+    def set_maxAmplitude(self, value):
+        self.maxAmplitude = value
+
+    def set_minAmplitude(self, value):
+        self.minAmplitude = value
+
+    def set_period(self, value):
+        self.period = value
+
+    def set_offset(self, value):
+        self.offset = value
+
+    def get_plot_widget(self):
+        return self.plt
+
+    def get_data(self):
+        return getattr(self, self.plot_func)(time.time())
+
+    def updateplot(self):
+        self.databuffer.append(self.get_data())
+        self.y[:] = self.databuffer
+        self.curve.setData(self.x, self.y)
+
+    def step(self, t=None):
+        return self.maxAmplitude
+
+    def sine(self, t):
+        return self.maxAmplitude * np.sin(
+            (2*np.pi/self.period)*t
+        ) + self.offset*np.ones_like(t)
+
+    def square(self, t):
+        return self.maxAmplitude * signal.square(
+            (2*np.pi/self.period)*t
+        ) + self.offset
+
+    def sawtooth(self, t):
+        return self.maxAmplitude * signal.sawtooth(
+            (2*np.pi/self.period)*t
+        ) + self.offset
+
+    def random(self, t):
+        return self.maxAmplitude
