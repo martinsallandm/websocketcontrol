5edfc40106abdfc836aa8b3699f72ed8b5f0e314
hugotallys <htmo@ic.ufal.com>
2022-04-08 11:55:17 -0300

    atualiza funções dos gŕaficos

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from PyQt6 import QtWidgets, QtCore
 import pyqtgraph as pg
 import sys  # We need sys so that we can pass argv to QApplication
-from random import randint
+import numpy as np
+from scipy import signal
 
 from main_window import Ui_MainWindow
 
@@ -11,14 +12,15 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
 
-        self.graphWidget = pg.PlotWidget(self.centralwidget)
+        self.graphWidget = pg.PlotWidget(self.centralWidget)
         self.graphWidget.setObjectName("graphWidget")
-        self.graphWidget.enableMouse(False)
-        self.horizontalLayout.addWidget(self.graphWidget)
-        self.horizontalLayout_2.addLayout(self.horizontalLayout)
+        self.graphWidgetLayout.addWidget(self.graphWidget)
 
-        self.x = list(range(100))  # 100 time points
-        self.y = [randint(0, 100) for _ in range(100)]  # 100 data points
+        self.plot_func = self.sin
+
+        self.n_points = 100
+        self.x = np.linspace(0, 5.0, self.n_points)
+        self.y = self.plot_func()
 
         self.graphWidget.setBackground('w')
 
@@ -26,20 +28,30 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.data_line = self.graphWidget.plot(self.x, self.y, pen=pen)
 
         self.timer = QtCore.QTimer()
-        self.timer.setInterval(100)
+        self.timer.setInterval(1000//60)
         self.timer.timeout.connect(self.update_plot_data)
         self.timer.start()
 
     def update_plot_data(self):
 
-        self.x = self.x[1:]  # Remove the first y element.
-        # Add a new value 1 higher than the last.
-        self.x.append(self.x[-1] + 1)
-
-        self.y = self.y[1:]  # Remove the first
-        self.y.append(randint(0, 100))  # Add a new random value.
-
+        self.x = np.roll(self.x, -1)  # Remove the first y element.
+        self.x[-1] = (self.x[-2] - self.x[-3]) + self.x[-2]
+        self.y = self.plot_func()
         self.data_line.setData(self.x, self.y)  # Update the data.
+
+    def step(self, amp=1.0):
+        return amp * np.ones_like(self.x)
+
+    def sin(self, amp=1.0, per=2*np.pi, offset=0.0):
+        return amp*np.sin((2*np.pi/per)*self.x) + offset*np.ones_like(self.x)
+
+    def pulse(self, amp=1.0, per=2*np.pi, offset=0.0):
+        off = offset*np.ones_like(self.x)
+        return signal.square((2*np.pi/per)*self.x) + off
+
+    def sawtooth(self, amp=1.0, per=2*np.pi, offset=0.0):
+        off = offset*np.ones_like(self.x)
+        return signal.sawtooth((2*np.pi/per)*self.x) + off
 
 
 app = QtWidgets.QApplication(sys.argv)
