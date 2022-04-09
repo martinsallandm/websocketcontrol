a1da0830eaa71323edaf5dd57a5831cd7ace3e65
jagra26 <jagra@ic.ufal.br>
2022-04-09 02:11:44 -0300

    thread works

--- test.py
+++ test.py
@@ -11,35 +11,40 @@ queue = Queue(maxsize=100)
 
 async def echo(websocket):
     while True:
-        await websocket.send("get references")
-        received = (await websocket.recv()).split(",")
-        # print("Reference: " + received[1])
-        await websocket.send("get outputs")
-        received = (await websocket.recv()).split(",")
-        print("Outputs: " + received[1])
-        queue.put(received)
+        try:
+            await websocket.send("get references")
+            received = (await websocket.recv()).split(",")
+            # print("Reference: " + received[1])
+            await websocket.send("get outputs")
+            received = (await websocket.recv()).split(",")
+            print("Outputs: " + received[1])
+            queue.put(received)
+        except Exception as e:
+            print(e)
+            return
+
+
+async def foo():
+    async with websockets.serve(echo, "localhost", 6660):
+        await asyncio.Future()
 
 
 def view():
     while True:
-        print("estou vivo")
-        if not queue.empty():
-            print("oi")
-            view = queue.get()
-            print(view)
+        try:
+            #print("estou vivo")
+            if not queue.empty():
+                print("oi")
+                view = queue.get()
+                print(view)
+        except Exception as e:
+            print(e)
+            return
 
 
-async def main():
-    async with websockets.serve(echo, "localhost", 6660):
-        await asyncio.Future()  # run forever
-
-    # server = websockets.serve(echo, "localhost", 6660)
-    # asyncio.ensure_future(server)
-    # asyncio.get_event_loop().run_forever()
-
-
-x = threading.Thread(target=view)
-x.start()
-
-y = threading.Thread(target=main)
+server = websockets.serve(echo, "localhost", 6660)
+asyncio.get_event_loop().run_until_complete(server)
+y = threading.Thread(target=view)
 y.start()
+asyncio.get_event_loop().run_forever()
+y.join()
